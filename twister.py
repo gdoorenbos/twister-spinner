19441b2ae3fe75334368cd053c25e271e0016c4e
Garrett Doorenbos <garrett.doorenbos@gmail.com>
2019-08-25 16:04:33 -0500

    feat(twister.py): finish python implementation

--- twister.py
+++ twister.py
@@ -7,5 +7,7 @@ appendages = ["hand", "foot"]
 options = [sides, appendages, colors]
 options = [' '.join(i) for i in product(*options)]
 
-opt_ind = randint(0, len(options)-1)
-print('{}: {}'.format(opt_ind, options[opt_ind]))
+while True:
+    opt_ind = randint(0, len(options)-1)
+    print('{}: {}'.format(opt_ind, options[opt_ind]), end='')
+    input()
